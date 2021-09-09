2f6fd215f62520034ff0ec601426070ceba94ea3
gcosmin1989 <gherghina.cosmin@yahoo.com>
2021-09-09 12:27:21 +0300

    Populate table

--- Dbh.php
+++ Dbh.php
@@ -24,38 +24,28 @@ class Dbh
 
 class Tops extends Dbh
 {
-    public $sales=[];
-    public $likes=[];
-    public $visits=[];
 
-    public function top_sales(){
-        $sql = "SELECT prod, sum(value) FROM actions WHERE type=1 group by prod ORDER BY `sum(value)` DESC LIMIT 100;";
+
+    public function getTop($type=[1,2,3], $period=['year','month','week']){
+        $sql = "select prod, $period(time),sum(value) from actions WHERE type=$type group by prod, $period(time) ORDER by sum(value) DESC LIMIT 100;";
         $result = mysqli_query($this->connect(), $sql);
         foreach ($result as $value) {
-            $this->sales[] = $value;
+            $tops[]=$value;
         }
-            return $this->sales;
+            return $tops;
+    }
+    public function createTable($type=[1,2,3], $period=['year','month','week']){
+        $sql ="Create TABLE tops(prod int(3) not null, time int(4) not null, sum int(10) not null)";
+        mysqli_query($this->connect(),$sql);
+        $sql="INSERT INTO tops (prod, time, sum) Select prod, $period(time),sum(value) from actions WHERE type=$type group by prod, $period(time) ORDER by sum(value) DESC LIMIT 100 ";
+        $result = mysqli_query($this->connect(),$sql);
+        return $result;
     }
 
-    public function top_visits(){
-        $sql = "SELECT prod, sum(value) FROM actions WHERE type=2 group by prod ORDER BY `sum(value)` DESC LIMIT 100;";
-        $result = mysqli_query($this->connect(), $sql);
-        foreach ($result as $value) {
-            $this->visits[] = $value;
-        }
-        return $this->visits;
-    }
-    public function top_likes(){
-        $sql = "SELECT prod, sum(value) FROM actions WHERE type=2 group by prod ORDER BY `sum(value)` DESC LIMIT 100;";
-        $result = mysqli_query($this->connect(), $sql);
-        foreach ($result as $value) {
-            $this->likes[] = $value;
-        }
-        return $this->likes;
-    }
 }
 
 $new = new Tops();
+$new->createTable(1,"month");
 
 
 
