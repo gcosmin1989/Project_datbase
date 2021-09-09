3e9822dc567a67b7f44250f512ba0b9a58fa48a6
gcosmin1989 <gherghina.cosmin@yahoo.com>
2021-09-09 15:06:37 +0300

    getTOPS

--- Dbh.php
+++ Dbh.php
@@ -25,27 +25,23 @@ class Dbh
 class Tops extends Dbh
 {
 
-
     public function getTop($type=[1,2,3], $period=['year','month','week']){
         $sql = "select prod, $period(time),sum(value) from actions WHERE type=$type group by prod, $period(time) ORDER by sum(value) DESC LIMIT 100;";
-        $result = mysqli_query($this->connect(), $sql);
-        foreach ($result as $value) {
-            $tops[]=$value;
-        }
-            return $tops;
-    }
-    public function createTable($type=[1,2,3], $period=['year','month','week']){
-        $sql ="Create TABLE tops(prod int(3) not null, time int(4) not null, sum int(10) not null)";
+        mysqli_query($this->connect(), $sql);
+        $sql ="DROP TABLE IF EXISTS tops";
         mysqli_query($this->connect(),$sql);
-        $sql="INSERT INTO tops (prod, time, sum) Select prod, $period(time),sum(value) from actions WHERE type=$type group by prod, $period(time) ORDER by sum(value) DESC LIMIT 100 ";
+        $sql= "CREATE TABLE tops(prod int(3) not null, $period int(4) not null, sum int(10) not null)";
+        mysqli_query($this->connect(),$sql);
+        $sql="INSERT INTO tops(prod, $period, sum) Select prod, $period(time),sum(value) from actions WHERE type=$type group by prod, $period(time) ORDER by sum(value) DESC LIMIT 100 ";
         $result = mysqli_query($this->connect(),$sql);
         return $result;
     }
 
+
 }
 
 $new = new Tops();
-$new->createTable(1,"month");
+$new->getTop(3,'week');
 
 
 
