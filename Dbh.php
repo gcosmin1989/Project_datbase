e59a022c781f68b3597802383b987abfae206445
gcosmin1989 <gherghina.cosmin@yahoo.com>
2021-09-10 13:29:03 +0300

    return Table

--- Dbh.php
+++ Dbh.php
@@ -2,6 +2,8 @@
 
 class Dbh
 {
+    public $type;
+    public $period;
     private $servername = "localhost";
     private $username = "root";
     private $password = "";
@@ -17,31 +19,55 @@ class Dbh
             return $conn;
         }
     }
-}
 
-
-
-
-class Tops extends Dbh
-{
-
-    public function getTop($type=[1,2,3], $period=['year','month','week']){
-        $sql = "select prod, $period(time),sum(value) from actions WHERE type=$type group by prod, $period(time) ORDER by sum(value) DESC LIMIT 100;";
+    public function getTop()
+    {
+        $sql = "select prod, $this->period(time),sum(value) from actions WHERE type=$this->type group by prod, $this->period(time) ORDER by sum(value) DESC LIMIT 100;";
         mysqli_query($this->connect(), $sql);
-        $sql ="DROP TABLE IF EXISTS tops";
-        mysqli_query($this->connect(),$sql);
-        $sql= "CREATE TABLE tops(prod int(3) not null, $period int(4) not null, sum int(10) not null)";
-        mysqli_query($this->connect(),$sql);
-        $sql="INSERT INTO tops(prod, $period, sum) Select prod, $period(time),sum(value) from actions WHERE type=$type group by prod, $period(time) ORDER by sum(value) DESC LIMIT 100 ";
-        $result = mysqli_query($this->connect(),$sql);
-        return $result;
     }
 
+    public function dropTable()
+    {
+        $sql = "DROP TABLE IF EXISTS tops";
+        mysqli_query($this->connect(), $sql);
+    }
 
+    public function createTable()
+    {
+        $sql = "CREATE TABLE tops(prod int(3) not null, $this->period int(4) not null, value int(10) not null)";
+        mysqli_query($this->connect(), $sql);
+    }
+
+    public function populateTable()
+    {
+        $sql = "INSERT INTO tops(prod, $this->period, value) Select prod, $this->period(time),sum(value) from actions WHERE type=$this->type group by prod, $this->period(time) ORDER by sum(value) DESC LIMIT 100 ";
+        mysqli_query($this->connect(), $sql);
+
+    }
+    public function createTops(){
+        $sql = "SELECT * FROM `tops` ORDER BY `tops`.`prod` ,`tops`.`$this->period` ASC;";
+        $results= mysqli_query($this->connect(), $sql);
+        return $results;
+    }
+
+    public function __construct($type=[1,2,3],$period=['week','month','year']){
+        $this->type=$type;
+        $this->period=$period;
+        $this->getTop();
+        $this->dropTable();
+        $this->createTable();
+        $this->populateTable();
+        $this->createTops();
+
+
+    }
 }
 
-$new = new Tops();
-$new->getTop(3,'week');
+
+
+
+
+
 
 
 
---
+++ Tops.php
@@ -0,0 +1,36 @@
+<?php
+require_once 'Dbh.php';
+
+class Tops
+{
+    public function ReturnArray()
+    {
+        $new = new Dbh(1, 'month');
+        $results = $new->createTops();
+        $records = array();
+        $period = $new->period;
+        while ($row = mysqli_fetch_array($results)) {
+            $records[] = $row;
+        }
+        echo "<table style='border: 1px solid black'>
+  <tr>
+    <th>Prod</th>
+    <th>$period</th>
+    <th>value</th>
+    <th>Stadiu</th>
+  </tr>";
+        foreach ($records as $row) {
+            echo "<tr>
+    <td>$row[prod]</td>
+    <td>$row[$period]</td>
+    <td>$row[value]</td>
+  </tr>
+    ";
+        }
+        echo "</tr>";
+        echo "</table>";
+    }
+}
+
+$new = new Tops();
+$new->ReturnArray();
